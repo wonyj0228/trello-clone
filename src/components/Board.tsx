6f9e9f2f79a50549e560a2850dd8cb8d39fca73f
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-07-19 23:25:34 +0900

    droppable snapshot

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,6 +2,18 @@ import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import DraggableCard from './DraggableCard';
 
+/**
+ * DroppableState -> snapshot
+ *
+ * isDraggingOver : boolean
+ * : draggable이 선택한 Droppable위에 드래깅되고(ing) 있는지 여부 확인
+ *
+ * draggingOverWith: ?DraggableId
+ * : Droppable위로 드래그하고있는 DraggableId
+ *
+ * draggingFromThisWith:
+ */
+
 const Wrapper = styled.div`
   width: 200px;
   padding: 20px 10px;
@@ -9,6 +21,9 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300;
+
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -17,6 +32,18 @@ const Title = styled.h2`
   margin-bottom: 20px;
   font-size: 18p;
 `;
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver ? 'pink' : props.draggingFromThisWith ? '' : 'blue'};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+`;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  draggingFromThisWith: boolean;
+}
 
 interface IBoardProps {
   toDos: string[];
@@ -28,17 +55,18 @@ function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div
-            style={{ backgroundColor: 'red' }}
-            ref={magic.innerRef}
-            {...magic.droppableProps}
+        {(provided, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            draggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
           >
             {toDos.map((toDo, idx) => (
               <DraggableCard key={toDo} idx={idx} toDo={toDo} />
             ))}
-            {magic.placeholder}
-          </div>
+            {provided.placeholder}
+          </Area>
         )}
       </Droppable>
     </Wrapper>
